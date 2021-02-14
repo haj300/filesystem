f69b8e7f1e305f243a802d52bf91f13135853cd5
haj300 <katja.lindeberg@gmail.com>
2021-02-14 17:06:01 +0100

    fetch data from html form

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Access the parse results as request.body
+app.post('/', function(req, res){
+    console.log(req.body.name);
+    console.log(req.body.description);
+});
+
 app.listen(3030, () => {
   console.log("server on 3030");
 });
